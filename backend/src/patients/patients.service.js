7bd37e50686d216e061f65463e8fb0acbab940ca
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-03-14 16:37:52 +0100

    feat: implement endpoint to get all patients

---
+++ backend/src/patients/patients.service.js
@@ -0,0 +1,38 @@
+import { prisma } from '../../prismaClient/client.js';
+
+export const getAll = async ({
+  firstName,
+  lastName,
+  pesel,
+  birthDate,
+  phone,
+  email,
+  address,
+  page = 1,
+  limit = 20,
+}) => {
+  const where = {};
+
+  if (firstName) where.firstName = { contains: firstName, mode: 'insensitive' };
+
+  if (lastName) where.lastName = { contains: lastName, mode: 'insensitive' };
+
+  if (pesel) where.pesel = pesel
+
+  if (birthDate) where.birthDate = new Date(birthDate);
+
+  if (phone) where.phone = { contains: phone, mode: 'insensitive' };
+
+  if (email) where.email = { contains: email, mode: 'insensitive' };
+
+  if (address) where.address = { contains: address, mode: 'insensitive' };
+
+  const patients = await prisma.patient.findMany({
+    where,
+    skip: (page - 1) * limit,
+    take: Number(limit),
+    orderBy: { createdAt: 'desc' },
+  });
+
+  return patients;
+};
