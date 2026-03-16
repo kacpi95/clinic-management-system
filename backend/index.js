f5ef55ca065e1b781639307a6d5c015fc6d3cf2a
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-03-16 13:03:12 +0100

    feat: add server Express with CORS

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+import patientsRoutes from './src/patients/patients.routes.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/patients', patientsRoutes);
+
+app.get('/api/test', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
