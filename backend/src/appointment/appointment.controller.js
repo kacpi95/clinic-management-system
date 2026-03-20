e2aaeba76ef0f52e45a57f9d609ecdcf3e43d910
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-03-20 23:34:36 +0100

    feat: add endpoint creation appointment

--- backend/src/appointment/appointment.controller.js
+++ backend/src/appointment/appointment.controller.js
@@ -50,3 +50,34 @@ export const getAppointmentById = async (req, res) => {
     return res.status(500).json({ message: 'Failed to fetch appointment' });
   }
 };
+
+export const addAppointment = async (req, res) => {
+  try {
+    const { patientId, doctorId, startTime, endTime, status, reason, notes } =
+      req.body;
+
+    const newAppointment = await appointmentService.add({
+      patientId,
+      doctorId,
+      startTime,
+      endTime,
+      status,
+      reason,
+      notes,
+    });
+
+    return res.status(201).json(newAppointment);
+  } catch (error) {
+    console.error(error);
+
+    if (error.message === 'Time slot already taken') {
+      return res.status(400).json({ message: error.message });
+    }
+
+    if (error.message === 'Invalid time range') {
+      return res.status(400).json({ message: error.message });
+    }
+
+    return res.status(500).json({ message: 'Failed to add new appointment' });
+  }
+};
