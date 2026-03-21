970c6341cf207ce6276cad26b698ffa03588d6a5
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-03-21 20:34:16 +0100

    feat: add appointments routes to Express

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 
 import patientsRoutes from './src/patients/patients.routes.js';
+import appointmentRoutes from './src/appointment/appointment.routes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/patients', patientsRoutes);
+app.use('/api/appointment', appointmentRoutes);
 
 app.get('/api/test', (req, res) => {
   res.json({ status: 'ok' });
