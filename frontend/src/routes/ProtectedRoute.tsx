7f3996868fb1dcdae19260a8aac0ca524671f13f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-03-30 12:51:27 +0200

    feat: implement protected route

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,12 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
-
 import { useAuth } from '../context/useAuth';
 
-export default function PublicRoute() {
+export default function ProtectedRoute() {
   const { token } = useAuth();
 
-  if (token) {
-    return <Navigate to='/' replace />;
+  if (!token) {
+    return <Navigate to='/login' replace />;
   }
+
   return <Outlet />;
 }
