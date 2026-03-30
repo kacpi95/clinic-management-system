2e0a1c666006b2efc82f4134f8652808fd9ac552
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-03-30 12:17:49 +0200

    feat: implement AuthProvider with localStorage

---
+++ frontend/src/context/AuthProvider.tsx
@@ -0,0 +1,45 @@
+import { AuthContext } from './AuthContext';
+
+import type { User, AuthResponse } from '../types/auth.types';
+import { useState, type ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function AuthProvider({ children }: Props) {
+  const [token, setToken] = useState<string | null>(() =>
+    localStorage.getItem('token'),
+  );
+  const [user, setUser] = useState<User | null>(() => {
+    try {
+      const localUser = localStorage.getItem('user');
+
+      return localUser ? JSON.parse(localUser) : null;
+    } catch {
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+      return null;
+    }
+  });
+
+  const login = ({ token, user }: AuthResponse) => {
+    setToken(token);
+    setUser(user);
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+  };
+
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
