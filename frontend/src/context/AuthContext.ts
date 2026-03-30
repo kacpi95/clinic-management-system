750e1d43676f1515663c0822cd1f9d93dd23b1b4
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-03-30 11:59:19 +0200

    feat: create AuthContext with ts types

---
+++ frontend/src/context/AuthContext.ts
@@ -0,0 +1,11 @@
+import type { User, AuthResponse } from './../types/auth.types';
+import { createContext } from 'react';
+
+interface AuthContextType {
+  token: string | null;
+  user: User | null;
+  login: (data: AuthResponse) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
