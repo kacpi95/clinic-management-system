2719ef514dbe181fd959611df6e456f36da6cdb0
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-04 22:50:46 +0200

    feat: add Routes to public components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import AuthProvider from './context/AuthProvider';
+import AuthLayout from './layouts/AuthLayout';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,29 +1,42 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
 
-// import { styles } from './AuthLayout.module.scss';
-
 export default function AuthLayout() {
   const location = useLocation();
   const isLoginPage = location.pathname === '/login';
+  const isRegisterPage = location.pathname === '/register';
+  const isHomePage = location.pathname === '/';
 
   return (
     <div>
       <h1>The Clinical Atelier</h1>
 
-      <Outlet />
-
-      <div>
-        {isLoginPage ? (
+      {isHomePage && (
+        <div>
           <p>
-            Nie masz konta w Clinical Atelier?
+            <Link to='/login'>Zaloguj się</Link>
+          </p>
+          <p>
             <Link to='/register'>Zarejestruj się</Link>
           </p>
-        ) : (
-          <p>
-            Masz już konto? <Link to='/login'>Zaloguj się</Link>
-          </p>
-        )}
-      </div>
+        </div>
+      )}
+
+      <Outlet />
+
+      {!isHomePage && (
+        <div>
+          {isLoginPage ? (
+            <p>
+              Nie masz konta w Clinical Atelier?{' '}
+              <Link to='/register'>Zarejestruj się</Link>
+            </p>
+          ) : isRegisterPage ? (
+            <p>
+              Masz już konto? <Link to='/login'>Zaloguj się</Link>
+            </p>
+          ) : null}
+        </div>
+      )}
     </div>
   );
 }
