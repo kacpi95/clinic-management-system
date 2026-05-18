2ad949aa4c29a599d3358888aab7b2f7fc170d47
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-05-18 18:57:34 +0200

    feat: add dashboard Promo with styles

--- frontend/src/features/dashboard/components/StatsGrid/StatsGrid.tsx
+++ frontend/src/features/dashboard/components/StatsGrid/StatsGrid.tsx
@@ -1,12 +1,14 @@
 import styles from './StatsGrid.module.scss';
 import MainCard from '../MainCard/MainCard';
 import SideCards from '../SideCards/SideCards';
+import Promo from '../Promo/Promo';
 
 export default function StatsGrid() {
   return (
     <section className={styles.wrapper}>
       <MainCard />
       <SideCards />
+      <Promo />
     </section>
   );
 }
