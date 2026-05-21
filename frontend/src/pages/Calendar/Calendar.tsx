8d00f5725ab5b811f0b2975640ecac153a82e7ef
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-05-21 18:39:51 +0200

    fix: Calendar page structure

--- frontend/src/pages/Calendar/Calendar.tsx
+++ frontend/src/pages/Calendar/Calendar.tsx
@@ -3,14 +3,13 @@ import styles from './Calendar.module.scss';
 export default function Calendar() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.leftSidebar}>
-        {/* CalendarFilters */}
-        {/* OnCallCard */}
-      </div>
-
-      <div className={styles.mainContent}>
-        {/* CalendarHeader */}
-        {/* CalendarGrid */}
+      <div className={styles.header}>{/* CalendarHeader */}</div>
+      <div className={styles.content}>
+        <div className={styles.leftSidebar}>
+          {/* CalendarFilters */}
+          {/* OnCallCard */}
+        </div>
+        <div className={styles.mainContent}>{/* CalendarGrid */}</div>
       </div>
     </div>
   );
