b8f96f6fa479c4bbbe315da4ae5e0a76f04560fc
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-07-12 19:58:00 +0200

    refactor: remove unused wrapper and styles

--- frontend/src/features/patients/pages/NewPatient/NewPatient.tsx
+++ frontend/src/features/patients/pages/NewPatient/NewPatient.tsx
@@ -1,13 +1,8 @@
 import PatientForm from '../../components/PatientForm/PatientForm';
-import styles from './NewPatient.module.scss';
 
 export default function NewPatient() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.header}>
-        <h1 className={styles.title}>Nowy pacjent</h1>
-      </div>
-
+    <div>
       <PatientForm />
     </div>
   );
