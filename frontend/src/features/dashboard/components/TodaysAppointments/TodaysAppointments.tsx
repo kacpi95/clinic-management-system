57355b4cbee19f3bf7773cda22065b391c6c3175
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-08-11 19:08:47 +0200

    feat: replace mock data with real appointments

--- frontend/src/features/dashboard/components/TodaysAppointments/TodaysAppointments.tsx
+++ frontend/src/features/dashboard/components/TodaysAppointments/TodaysAppointments.tsx
@@ -1,15 +1,21 @@
 import styles from './TodaysAppointments.module.scss';
-import { useAppointments } from '../../../appointments/hooks/useAppointments';
 import patientOne from '../../../../assets/patient-1.png';
+import { getTodayAppointments } from '../../utils/getTodayAppointments';
+import { useCalendarAppointments } from '../../../calendar/hooks/useCalendarAppointments';
+import { useNavigate } from 'react-router-dom';
 
 export default function TodaysAppointments() {
-  const { appointments, isLoading, error } = useAppointments();
+  const navigate = useNavigate();
 
-  const today = new Date().toDateString();
+  const { calendarAppointments, isLoading, error } = useCalendarAppointments();
 
-  const todaysAppointments = appointments.filter((appointment) => {
-    return new Date(appointment.startTime).toDateString() === today;
-  });
+  const todaysAppointments = getTodayAppointments(calendarAppointments);
+
+  const statusLabels = {
+    PLANNED: 'Zaplanowana',
+    COMPLETED: 'Zakończona',
+    CANCELED: 'Anulowana',
+  };
 
   if (isLoading) {
     return <p>Ładowanie wizyt...</p>;
@@ -23,45 +29,59 @@ export default function TodaysAppointments() {
     <section className={styles.wrapper}>
       <div className={styles.header}>
         <h2 className={styles.title}>Dzisiejsze wizyty</h2>
-        <button className={styles.linkButton}>Zobacz wszystkie</button>
+        <button
+          onClick={() => navigate('/dashboard/calendar')}
+          className={styles.linkButton}
+        >
+          Zobacz wszystkie
+        </button>
       </div>
+      {todaysAppointments.length === 0 ? (
+        <p className={styles.empty}>Brak wizyt na dziś.</p>
+      ) : (
+        <ul className={styles.list}>
+          {todaysAppointments.map((appointment) => {
+            const time = new Date(appointment.startTime).toLocaleTimeString(
+              'pl-PL',
+              {
+                hour: '2-digit',
+                minute: '2-digit',
+              },
+            );
 
-      <ul className={styles.list}>
-        {todaysAppointments.map((appointment) => {
-          const time = new Date(appointment.startTime).toLocaleTimeString(
-            'pl-PL',
-            {
-              hour: '2-digit',
-              minute: '2-digit',
-            },
-          );
+            return (
+              <li key={appointment.id} className={styles.item}>
+                <article
+                  onClick={() =>
+                    navigate(`/dashboard/patients/${appointment.patientId}`)
+                  }
+                  className={styles.card}
+                >
+                  <div className={styles.time}>
+                    <span>{time}</span>
+                  </div>
+                  <div className={styles.avatar}>
+                    <img src={patientOne} alt='Zdjęcie pacjenta' />
+                  </div>
+                  <div className={styles.content}>
+                    <h3>
+                      {appointment.patient?.firstName}{' '}
+                      {appointment.patient?.lastName}
+                    </h3>
+                    <p>{appointment.reason}</p>
+                  </div>
 
-          return (
-            <li key={appointment.id} className={styles.item}>
-              <article className={styles.card}>
-                <div className={styles.time}>
-                  <span>{time}</span>
-                </div>
-
-                <div className={styles.avatar}>
-                  <img src={patientOne} alt='Zdjęcie pacjenta' />
-                </div>
-
-                <div className={styles.content}>
-                  <h3>
-                    {appointment.patient?.firstName}{' '}
-                    {appointment.patient?.lastName}
-                  </h3>
-
-                  <p>{appointment.reason}</p>
-                </div>
-
-                <div className={styles.status}>{appointment.status}</div>
-              </article>
-            </li>
-          );
-        })}
-      </ul>
+                  <div
+                    className={`${styles.status} ${styles[appointment.status.toLowerCase()]}`}
+                  >
+                    {statusLabels[appointment.status]}
+                  </div>
+                </article>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </section>
   );
 }
